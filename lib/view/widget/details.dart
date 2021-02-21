206934c358fb3ca6cd8640e742b6fa1a7b33c27e
jeffersonamaral <jefferson.amarals@gmail.com>
2021-02-21 16:59:14 -0300

    Adição de Ano, Duração, Gêneros e Descrição na tela de Detelhe.

--- lib/view/widget/details.dart
+++ lib/view/widget/details.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:buscador_filmes/model/genre.dart';
 import 'package:buscador_filmes/model/movie.dart';
 import 'package:buscador_filmes/view/widget/please_wait.dart';
 import 'package:flutter/material.dart';
@@ -33,13 +34,41 @@ class Details extends StatelessWidget {
     return detailedMovie;
   }
 
+  List<Widget> _createGenresRow(Movie movie) {
+    List<Widget> widgets = List<Widget>();
+
+    for (Genre genre in movie.genres) {
+      widgets.add(
+          Container(
+              padding: EdgeInsets.fromLTRB(20, 8, 20, 8),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    color: Color(0xfff1f3f5),
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(8))
+              ),
+              child: Text(
+                genre.name.toUpperCase(),
+                style: TextStyle(
+                    color: Color(0xff5e6770),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                ),
+              )
+          )
+      );
+    }
+
+    return widgets;
+  }
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async => false,
       child: Scaffold(
-          floatingActionButton:
-          Padding(
+          floatingActionButton: Padding(
               padding: EdgeInsets.only(top: 30),
               child: FloatingActionButton.extended(
                 onPressed: () => Navigator.of(context).pop(),
@@ -75,118 +104,183 @@ class Details extends StatelessWidget {
                   if (snapshot.hasError) {
                     result = ErrorMessage();
                   } else {
-                    result = Container(
-                        color: Colors.white, //Color(0xfff5f5f5),
-                        width: MediaQuery.of(context).size.width,
+                    result = SingleChildScrollView(
                         child: Container(
-                          padding: EdgeInsets.only(top: 200),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Stack(
+                            padding: EdgeInsets.only(left: 25, right: 25),
+                            color: Colors.white, //Color(0xfff5f5f5),
+                            width: MediaQuery.of(context).size.width,
+                            child: Container(
+                                padding: EdgeInsets.only(top: 180),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
                                   children: [
-                                    Align(
-                                        alignment: Alignment.center,
-                                        child: ClipRRect(
-                                          borderRadius: BorderRadius.circular(16),
-                                          child: Image.network(
-                                            _posterBaseUrl + snapshot.data.posterPath,
-                                            width: MediaQuery.of(context).size.width * 0.6,
+                                    Stack(
+                                      children: [
+                                        Align(
+                                            alignment: Alignment.center,
+                                            child: ClipRRect(
+                                              borderRadius: BorderRadius.circular(16),
+                                              child: Image.network(
+                                                _posterBaseUrl + snapshot.data.posterPath,
+                                                width: MediaQuery.of(context).size.width * 0.6,
+                                              ),
+                                            )
+                                        ),
+                                        Align(
+                                          alignment: Alignment.bottomCenter,
+                                          child: Text(
+                                            'NOS CINEMAS',
+                                            style: TextStyle(
+                                                color: Colors.yellow,
+                                                fontWeight: FontWeight.bold
+                                            ),
                                           ),
-                                        )
+                                        ),
+                                      ],
                                     ),
-                                    Align(
-                                      alignment: Alignment.bottomCenter,
-                                      child: Text(
-                                        'NOS CINEMAS',
-                                        style: TextStyle(
-                                            color: Colors.yellow,
-                                            fontWeight: FontWeight.bold
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.only(top: 50, bottom: 50),
-                                  child: Row(
-                                    crossAxisAlignment: CrossAxisAlignment.baseline,
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Text(
-                                        snapshot.data.voteAverage,
-                                        style: TextStyle(
-                                            color: Color(0xff00384c),
-                                            fontSize: 35,
-                                            fontWeight: FontWeight.bold
-                                        ),
-                                      ),
-                                      Text(
-                                        ' / 10',
-                                        style: TextStyle(
-                                            color: Color(0xff868e96),
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.bold
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                                Text(
-                                  snapshot.data.title.toUpperCase(),
-                                  style: TextStyle(
-                                      color: Color(0xff00384c),
-                                      fontSize: 22,
-                                      fontWeight: FontWeight.bold
-                                  ),
-                                ),
-                                Padding(
-                                    padding: EdgeInsets.only(top: 18, bottom: 36),
-                                    child: Text(
-                                      'Título original: ${snapshot.data.originalTitle}',
-                                      style: TextStyle(
-                                          color: Colors.blueGrey,
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.normal
-                                      ),
-                                    )
-                                ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Container(
-                                      padding: EdgeInsets.fromLTRB(20, 13, 20, 13),
-                                      decoration: BoxDecoration(
-                                          color: Colors.red, //Color(0xfff1f3f5),
-                                          border: Border.all(
-                                            color: Colors.red//Color(0xfff1f3f5),
-                                          ),
-                                          borderRadius: BorderRadius.all(Radius.circular(10))
-                                      ),
+                                    Padding(
+                                      padding: EdgeInsets.only(top: 40, bottom: 40),
                                       child: Row(
+                                        crossAxisAlignment: CrossAxisAlignment.baseline,
+                                        mainAxisAlignment: MainAxisAlignment.center,
                                         children: [
                                           Text(
-                                            'Ano: ',
+                                            snapshot.data.voteAverage,
                                             style: TextStyle(
-                                                color: Color(0xff868e96),
-                                                fontSize: 20,
+                                                color: Color(0xff00384c),
+                                                fontSize: 35,
                                                 fontWeight: FontWeight.bold
                                             ),
                                           ),
                                           Text(
-                                            snapshot.data.year,
+                                            ' / 10',
                                             style: TextStyle(
-                                                color: Color(0xff343a40),
+                                                color: Color(0xff868e96),
                                                 fontSize: 20,
                                                 fontWeight: FontWeight.bold
                                             ),
                                           )
                                         ],
                                       ),
+                                    ),
+                                    Text(
+                                      snapshot.data.title.toUpperCase(),
+                                      style: TextStyle(
+                                          color: Color(0xff00384c),
+                                          fontSize: 22,
+                                          fontWeight: FontWeight.bold,
+                                          fontFamily: 'Arial'
+                                      ),
+                                    ),
+                                    Padding(
+                                        padding: EdgeInsets.only(top: 10, bottom: 40),
+                                        child: Text(
+                                          'Título original: ${snapshot.data.originalTitle}',
+                                          style: TextStyle(
+                                              color: Colors.blueGrey,
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.normal
+                                          ),
+                                        )
+                                    ),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: [
+                                        Container(
+                                          padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                                          decoration: BoxDecoration(
+                                              color: Color(0xfff1f3f5),
+                                              border: Border.all(
+                                                color: Color(0xfff1f3f5),
+                                              ),
+                                              borderRadius: BorderRadius.all(Radius.circular(8))
+                                          ),
+                                          child: Row(
+                                            children: [
+                                              Text(
+                                                'Ano: ',
+                                                style: TextStyle(
+                                                    color: Color(0xff868e96),
+                                                    fontSize: 20,
+                                                    fontWeight: FontWeight.bold
+                                                ),
+                                              ),
+                                              Text(
+                                                snapshot.data.year,
+                                                style: TextStyle(
+                                                    color: Color(0xff343a40),
+                                                    fontSize: 20,
+                                                    fontWeight: FontWeight.bold
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                        SizedBox.fromSize(size: Size(20, 20)),
+                                        Container(
+                                          padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                                          decoration: BoxDecoration(
+                                              color: Color(0xfff1f3f5),
+                                              border: Border.all(
+                                                color: Color(0xfff1f3f5),
+                                              ),
+                                              borderRadius: BorderRadius.all(Radius.circular(8))
+                                          ),
+                                          child: Row(
+                                            children: [
+                                              Text(
+                                                'Duração: ',
+                                                style: TextStyle(
+                                                    color: Color(0xff868e96),
+                                                    fontSize: 20,
+                                                    fontWeight: FontWeight.bold
+                                                ),
+                                              ),
+                                              Text(
+                                                '1h 20 min',
+                                                style: TextStyle(
+                                                    color: Color(0xff343a40),
+                                                    fontSize: 20,
+                                                    fontWeight: FontWeight.bold
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                    SizedBox.fromSize(size: Size(20, 15)),
+                                    Wrap(
+                                      crossAxisAlignment: WrapCrossAlignment.center,
+                                      spacing: 8,
+                                      runSpacing: 8,
+                                      alignment: WrapAlignment.spaceEvenly,
+                                      children: _createGenresRow(snapshot.data),
+                                    ),
+                                    SizedBox.fromSize(size: Size(20, 75)),
+                                    Align(
+                                      alignment: Alignment.bottomLeft,
+                                      child: Text(
+                                        'Descrição',
+                                        style: TextStyle(
+                                          color: Colors.blueGrey,
+                                          fontSize: 20,
+                                        ),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.only(top: 10),
+                                      child: Text(
+                                        snapshot.data.overview,
+                                        style: TextStyle(
+                                            color: Colors.grey[700],
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold
+                                        ),
+                                      ),
                                     )
                                   ],
                                 )
-                              ],
                             )
                         )
                     );
